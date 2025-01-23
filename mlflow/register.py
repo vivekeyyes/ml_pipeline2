edaf9f5d48c00fe7193bd6e7af0eea5948596690
vivek <vivek.sadarahalli@eyyes.com>
2025-01-23 15:00:17 +0100

    update register.py

--- mlflow/register.py
+++ mlflow/register.py
@@ -12,7 +12,7 @@ import time
 mlflow_server_command = [
     "mlflow", "server",
     "--backend-store-uri", "file:///D:/Automation_pipeline/mlruns",
-    "--default-artifact-root", r"D:\Automation_pipeline\artifacts",
+    "--default-artifact-root", "file:///D:/Automation_pipeline/artifacts",
     "--host", "0.0.0.0",
     "--port", "5000"
 ]
